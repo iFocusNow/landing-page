3df303a17a04084ccab27a64042a780ea30726a6
novarios1506 <frios1506@gmail.com>
2022-10-23 20:13:20 -0500

    Version 1 - Focus-landing-page

--- js/script.js
+++ js/script.js
@@ -1,13 +1,39 @@
+(function(){
+    const sliders = [...document.querySelectorAll('.slider__body')];
+    const arrowNext = document.querySelector('#next');
+    const arrowBefore = document.querySelector('#before');
+    let value;
+
+    arrowNext.addEventListener('click', ()=>changePosition(1));
+
+    arrowBefore.addEventListener('click', ()=>changePosition(-1));
+
+    function changePosition(change){
+        const currentElement = Number(document.querySelector('.slider__body--show').dataset.id);
+        // 4+1 = 5
+        value = currentElement;
+        value+= change;
+
+        console.log(sliders.length)
+        if(value === 0 || value == sliders.length+1){
+            value = value === 0 ? sliders.length : 1;
+        }
+
+        sliders[currentElement-1].classList.toggle('slider__body--show');
+        sliders[value-1].classList.toggle('slider__body--show');
+    }
+})()
+
 window.onscroll = function(){
 
     scroll = document.documentElement.scrollTop;
 
-    header = document.getElementById("header");
+    hero = document.getElementById("nav");
 
     if (scroll > 20){
-        header.classList.add('nav_mod');
+        hero.classList.add('nav_mod');
     }else if (scroll < 20){
-        header.classList.remove('nav_mod');
+        hero.classList.remove('nav_mod');
     }
 
 }
